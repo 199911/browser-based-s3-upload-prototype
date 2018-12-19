7c4192f928b46b0eab060d22d41f7e59c6b99ac1
KU Lok Sun <sundaykuloksun@gmail.com>
2018-12-19 17:36:38 +0800

    feat: implement upload part logic in LargeFileInput.js

--- src/LargeFileInput.js
+++ src/LargeFileInput.js
@@ -5,8 +5,8 @@
 import React, { Component } from 'react';
 import './LargeFileInput.css';
 
-// Set size of each chunk to 32 MB
-const sliceSize = 32 * 1024 * 1024;
+// Set size of each chunk to 64 MB
+const sliceSize = 64 * 1024 * 1024;
 
 class LargeFileInput extends Component {
   constructor(props) {
@@ -47,7 +47,44 @@ class LargeFileInput extends Component {
       // parses response to JSON
       .then(response => response.json());
 
-    console.log(response);
+    const { UploadId: uploadId } = response;
+
+    // TODO: Re-try logic
+    // TODO: Limit the number of parallel upload, avoid timeout problem
+    const promises = parts.map(async (part, index) => {
+      // Get signed upload part url
+      const partNum = index + 1;
+      const { url } = await fetch(
+        `//localhost:3001/uploads/${uploadId}/parts/${partNum}`,
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+          },
+        }
+      )
+        // parses response to JSON
+        .then(response => response.json());
+
+      // Upload part
+      const result = await fetch(url, {
+        // signed put-object url must be triggered by PUT method
+        method: 'PUT',
+        body: part,
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      if (result.ok) {
+        console.log(await result.text());
+      } else {
+        throw new Error(`Failure: part ${partNum}`);
+      }
+    });
+
+    Promise.all(promises);
+
   }
 
   render() {
