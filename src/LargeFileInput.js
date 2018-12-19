8f1e7adb298213b6184e372c7134af3bafa25a18
KU Lok Sun <sundaykuloksun@gmail.com>
2018-12-19 16:58:12 +0800

    feat: implement chunk file logic

--- src/LargeFileInput.js
+++ src/LargeFileInput.js
@@ -5,6 +5,9 @@
 import React, { Component } from 'react';
 import './LargeFileInput.css';
 
+// Set size of each chunk to 32 MB
+const sliceSize = 32 * 1024 * 1024;
+
 class LargeFileInput extends Component {
   constructor(props) {
     super(props);
@@ -14,6 +17,21 @@ class LargeFileInput extends Component {
 
   async handleSubmit(event) {
     event.preventDefault();
+
+    const file = this.fileInput.current.files[0];
+    const size = file.size;
+
+    // Chunk the file
+    const parts = [];
+    let start = 0;
+    let end = sliceSize;
+    while(start < size) {
+      parts.push(file.slice(start, end));
+      start = end;
+      end += sliceSize;
+    }
+
+    console.log(parts);
 
     // Init upload process
     const response = await fetch(
